3230e7b766568ae25f67b6627c82b2b7e882037d
canknbr <cankanbur1@gmail.com>
2023-06-05 18:09:30 +0300

    Initial Commit

---
+++ ChatWave/ChatWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWaveApp.swift
+//  ChatWave
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
